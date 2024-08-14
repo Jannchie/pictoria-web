4859a581578fd8b4d96ba1b7436fa1a9069785d5
Jianqi Pan <jannchie@gmail.com>
2024-08-14 23:41:11 +0900

    feat(style): better unocss roku ui config

--- uno.config.ts
+++ uno.config.ts
@@ -1,20 +1,8 @@
-import * as fs from 'node:fs'
 import { defineConfig } from 'unocss'
 import { rokuPreset } from '@roku-ui/preset'
-
-let file = ''
-try {
-  file = fs.readFileSync('node_modules/@roku-ui/vue/dist/index.js', 'utf-8')
-}
-catch {
-  console.error('Failed to read file')
-}
 
 export default defineConfig({
   presets: [
     rokuPreset(),
   ],
-  content: {
-    inline: [file],
-  },
 })
