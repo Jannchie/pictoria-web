bdb4b324a69392a6fd9d86a7583a4ef944d5a463
Jianqi Pan <jannchie@gmail.com>
2024-11-13 02:10:32 +0900

    refactor(exports): remove unused useDropToUpload export

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,4 +1,3 @@
 export { useClientHeight, useClientWidth } from './useClientHeight'
-export { useDropToUpload } from './useDropToUpload'
 export { usePostQuery } from './usePostQuery'
 export { useWatchRoute } from './useWatchRoute'
